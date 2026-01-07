e1a70c5129568c1280f47a315133ac18a18d28b5
geethasamynathan <geethasamynathan2011@gmail.com>
2026-01-07 10:47:02 +0530

    Add files via upload

---
+++ week 3/Day 5/result.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+echo "<h3>Submitted Data</h3>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Message:</strong> $message</p>";
+?>
