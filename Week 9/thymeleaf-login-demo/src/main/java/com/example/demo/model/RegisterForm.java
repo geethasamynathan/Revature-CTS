4f5cfecbfd3fb92d419a061798e9cb6f5541d092
geethasamynathan <geethasamynathan2011@gmail.com>
2026-02-11 12:35:30 +0530

    Add files via upload

---
+++ Week 9/thymeleaf-login-demo/src/main/java/com/example/demo/model/RegisterForm.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterForm {
+
+	@NotBlank(message="Name is Required")
+	private String name;
+	
+	@Email(message="Enter a valid Email")
+	@NotBlank(message="Email is Required")
+	private String email;
+	
+	@Size(min =4, message="Password must be atleast 4 characters")
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
+
+
